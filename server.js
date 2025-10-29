4f9f0d71469127c2fcfe209b26346531dfb372d7
mositomalehloa-dotcom <mositomalehloa@gmail.com>
2025-10-29 13:55:30 +0200

    Update db connection and CORS for Render deployment

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-// backend/server.js
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const feedbackRoutes = require('./routes/feedback');
-require('dotenv').config(); // MUST BE AT THE TOP!
 
 const app = express();
+const pool = require('./db'); 
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 app.use(cors({
   origin: ['http://localhost:3000' ,'http://localhost:3001'],
   credentials: true
 }));
 app.use(express.json());
 
-// Routes
+
 app.use('/api/feedback', feedbackRoutes);
 
-// Test route
 app.get('/', (req, res) => {
   res.send('Student Feedback API is running!');
 });
 
-// Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
